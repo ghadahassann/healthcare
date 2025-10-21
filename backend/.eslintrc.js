96152c1dd23ecabde035303343f2b54f9c855101
ghadahassann <alhamdullahalhamdullah93@gmail.com>
2025-10-22 02:01:21 +0300

    edited backend eslint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    // Rules here
+  }
+};
