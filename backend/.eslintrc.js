fac6ab1d5294d63c1aacd26edb1c4b549685e51f
ghadahassann <alhamdullahalhamdullah93@gmail.com>
2025-10-22 02:04:31 +0300

    edited backend eslint 2

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -2,12 +2,25 @@ module.exports = {
   env: {
     node: true,
     jest: true,
+    es6: true
   },
   extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 2020,
+    sourceType: 'module'
   },
   rules: {
-    // Rules here
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'no-undef': 'off'
+  },
+  globals: {
+    Promise: 'readonly',
+    console: 'readonly',
+    process: 'readonly',
+    require: 'readonly',
+    module: 'readonly',
+    __dirname: 'readonly',
+    __filename: 'readonly'
   }
 };
